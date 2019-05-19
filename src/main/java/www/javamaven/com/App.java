71bda76523d4051cdfcedc1dbeaa69f3af78fc30
zhc <42126087+tmac263@users.noreply.github.com>
2019-05-19 12:00:18 +0800

    Update App.java

--- src/main/java/www/javamaven/com/App.java
+++ src/main/java/www/javamaven/com/App.java
@@ -15,6 +15,6 @@ public class App
         System.out.println("f = " + f);
         System.out.println( "Hello World!" );
         System.out.println( "Hello World!" );
-
+        System.out.println( "周聪" );
     }
 }
